5ce11f6992cf376c165a45194114ae019fd57366
Shannon <Jacob.Shannon@craneae.com>
2018-05-29 17:16:43 -0700

    Four functional graphs.

--- design_electronics/landing_page/static/js/plotBode.js
+++ design_electronics/landing_page/static/js/plotBode.js
@@ -5,7 +5,8 @@
 //  phase_data - This is a list of values that is the same size as the range variable that defines the 
 //                the phase data of the transfer function in degrees to be plotted.
 
-function BodePlot(mags, mags_min, mags_max, phases, phases_min, phases_max, mag_plot_div, phase_plot_div){
+function BodePlot(mags, mags_min, mags_max, phases, phases_min, phases_max, mag_plot_div, phase_plot_div)
+{
     this.mags= mags;
     this.mags_min = mags_min;
     this.mags_max = mags_max;
@@ -99,12 +100,6 @@ function BodePlot(mags, mags_min, mags_max, phases, phases_min, phases_max, mag_
                         .y(function(d) { return this.magY(d.y); })
                         .interpolate("linear");
 
-    this.magZoom = d3.behavior.zoom()
-                        .x(this.x)
-                        .y(this.magY)
-                        .scaleExtent([1, 1])
-                        .on("zoom",this.redraw);
-
     // Variable that sets the phase data y axis data range.
     // Dynamically re-sizes based upon transfer function maximum and minimum.
     if(this.phases.length > 0){
@@ -171,6 +166,12 @@ function BodePlot(mags, mags_min, mags_max, phases, phases_min, phases_max, mag_
         .y(function(d) { return this.phsY(d.y); })
         .interpolate("linear");
 
+    this.magZoom = d3.behavior.zoom()
+        .x(this.x)
+        .y(this.magY)
+        .scaleExtent([1, 1])
+        .on("zoom",this.redraw);
+
     this.phsZoom = d3.behavior.zoom()
         .x(this.x)
         .y(this.phsY)
@@ -192,196 +193,209 @@ function BodePlot(mags, mags_min, mags_max, phases, phases_min, phases_max, mag_
         .append("g")
         .attr("transform","translate(" + this.margin.left + "," + this.margin.top + ")")
         .call(this.phsZoom);
+}
 
-    // Append x grid
-    this.plotMag.append("g")
-        .attr("class","x grid")
-        .attr("transform","translate(0," + this.height + ")")
-        .call(this.xGrid);
+BodePlot.prototype = {
+    constructor: BodePlot,
+    createGraphs: function()
+    {
+        // Append x grid
+        this.plotMag.append("g")
+            .attr("class","x grid")
+            .attr("transform","translate(0," + this.height + ")")
+            .call(this.xGrid);
 
-    // Append y grid
-    this.plotMag.append("g")
-        .attr("class","y grid")
-        .call(this.magYGrid);
+        // Append y grid
+        this.plotMag.append("g")
+            .attr("class","y grid")
+            .call(this.magYGrid);
 
-    // Append x axis
-    this.plotMag.append("g")
-        .attr("class","x1 axis")
-        .attr("transform","translate(0," + this.height + ")")
-        .call(this.magXAxis1);
+        // Append x axis
+        this.plotMag.append("g")
+            .attr("class","x1 axis")
+            .attr("transform","translate(0," + this.height + ")")
+            .call(this.magXAxis1);
 
-    // Append additional X axis
-    this.plotMag.append("g")
-        .attr("class","x2 axis")
-        .attr("transform","translate(" + [0, 0] + ")")
-        .call(this.magXAxis2);
+        // Append additional X axis
+        this.plotMag.append("g")
+            .attr("class","x2 axis")
+            .attr("transform","translate(" + [0, 0] + ")")
+            .call(this.magXAxis2);
 
-    // Append y axis
-    this.plotMag.append("g")
-        .attr("class","y1 axis")
-        .call(this.magYAxis1);
+        // Append y axis
+        this.plotMag.append("g")
+            .attr("class","y1 axis")
+            .call(this.magYAxis1);
 
-    // Append additional y axis
-    this.plotMag.append("g")
-        .attr("class","y2 axis")
-        .attr("transform","translate(" + [this.width, 0] + ")")
-        .call(this.magYAxis2);
+        // Append additional y axis
+        this.plotMag.append("g")
+            .attr("class","y2 axis")
+            .attr("transform","translate(" + [this.width, 0] + ")")
+            .call(this.magYAxis2);
 
-    // Add x axis label
-    this.plotMag.append("text")
-        .attr("transform","translate(" + (this.width / 2) + "," + (this.height + this.margin.bottom - 5) + ")")
-        .style("font-size","15")
-        .style("text-anchor","middle")
-        .text("Frequency [Hz]");
+        // Add x axis label
+        this.plotMag.append("text")
+            .attr("transform","translate(" + (this.width / 2) + "," + (this.height + this.margin.bottom - 5) + ")")
+            .style("font-size","15")
+            .style("text-anchor","middle")
+            .text("Frequency [Hz]");
 
-    // Add y axis label
-    this.plotMag.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y",0 - this.margin.left)
-        .attr("x",0 - (this.height / 2))
-        .attr("dy", "1em")
-        .style("font-size","15")
-        .style("text-anchor", "middle")
-        .text("Magnitude [dB]");
+        // Add y axis label
+        this.plotMag.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y",0 - this.margin.left)
+            .attr("x",0 - (this.height / 2))
+            .attr("dy", "1em")
+            .style("font-size","15")
+            .style("text-anchor", "middle")
+            .text("Magnitude [dB]");
 
-    // Clip path
-    this.plotMag.append("defs").append("clipPath")
-        .attr("id", "clip")
-        .append("rect")
-        .attr("width", this.width)
-        .attr("height", this.height);
+        // Clip path
+        this.plotMag.append("defs").append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("width", this.width)
+            .attr("height", this.height);
 
-    this.plotMag.append("rect")
-        .attr("width", this.width)
-        .attr("height", this.height);
+        this.plotMag.append("rect")
+            .attr("width", this.width)
+            .attr("height", this.height);
 
-    // Append x grid
-    this.plotPhs.append("g")
-        .attr("class","x grid")
-        .attr("transform","translate(0," + this.height + ")")
-        .call(this.xGrid);
+        // Append x grid
+        this.plotPhs.append("g")
+            .attr("class","x grid")
+            .attr("transform","translate(0," + this.height + ")")
+            .call(this.xGrid);
 
-    // Append y grid
-    this.plotPhs.append("g")
-        .attr("class","y grid")
-        .call(this.phsYGrid);
+        // Append y grid
+        this.plotPhs.append("g")
+            .attr("class","y grid")
+            .call(this.phsYGrid);
 
-    // Append x axis
-    this.plotPhs.append("g")
-        .attr("class","x1 axis")
-        .attr("transform","translate(0," + this.height + ")")
-        .call(this.phsXAxis1);
+        // Append x axis
+        this.plotPhs.append("g")
+            .attr("class","x1 axis")
+            .attr("transform","translate(0," + this.height + ")")
+            .call(this.phsXAxis1);
 
-    // Append additional X axis
-    this.plotPhs.append("g")
-        .attr("class","x2 axis")
-        .attr("transform","translate(" + [0, 0] + ")")
-        .call(this.phsXAxis2);
+        // Append additional X axis
+        this.plotPhs.append("g")
+            .attr("class","x2 axis")
+            .attr("transform","translate(" + [0, 0] + ")")
+            .call(this.phsXAxis2);
 
-    // Append y axis
-    this.plotPhs.append("g")
-        .attr("class","y1 axis")
-        .call(this.phsYAxis1);
+        // Append y axis
+        this.plotPhs.append("g")
+            .attr("class","y1 axis")
+            .call(this.phsYAxis1);
 
-    // Append additional y axis
-    this.plotPhs.append("g")
-        .attr("class","y2 axis")
-        .attr("transform","translate(" + [this.width, 0] + ")")
-        .call(this.phsYAxis2);
+        // Append additional y axis
+        this.plotPhs.append("g")
+            .attr("class","y2 axis")
+            .attr("transform","translate(" + [this.width, 0] + ")")
+            .call(this.phsYAxis2);
 
-    // Add x axis label  
-    this.plotPhs.append("text")
-        .attr("transform","translate(" + (this.width / 2) + "," + (this.height + this.margin.bottom - 5) + ")")
-        .style("font-size","15")
-        .style("text-anchor","middle")
-        .text("Frequency [Hz]");
+        // Add x axis label  
+        this.plotPhs.append("text")
+            .attr("transform","translate(" + (this.width / 2) + "," + (this.height + this.margin.bottom - 5) + ")")
+            .style("font-size","15")
+            .style("text-anchor","middle")
+            .text("Frequency [Hz]");
 
-    // Add y axis label
-    this.plotPhs.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y",0 - this.margin.left)
-        .attr("x",0 - (this.height / 2))
-        .attr("dy", "1em")
-        .style("font-size","15")
-        .style("text-anchor", "middle")
-        .text("Phase [deg]");
+        // Add y axis label
+        this.plotPhs.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y",0 - this.margin.left)
+            .attr("x",0 - (this.height / 2))
+            .attr("dy", "1em")
+            .style("font-size","15")
+            .style("text-anchor", "middle")
+            .text("Phase [deg]");
 
-    // Clip path
-    this.plotPhs.append("defs").append("clipPath")
-        .attr("id", "clip")
-        .append("rect")
-        .attr("width", this.width)
-        .attr("height", this.height);
+        // Clip path
+        this.plotPhs.append("defs").append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("width", this.width)
+            .attr("height", this.height);
 
-    this.plotPhs.append("rect")
-        .attr("width", this.width)
-        .attr("height", this.height);
-
-    this.redraw = function(){
-        console.log("test");
-        this.plotMag.select(".x1.axis").call(this.magXAxis1);
-        this.plotMag.select(".y1.axis").call(this.magYAxis1);
-        this.plotMag.select(".x2.axis").call(this.magXAxis2);
-        this.plotMag.select(".y2.axis").call(this.magYAxis2);
-        this.plotMag.select(".x.grid").call(this.xGrid);
-        this.plotMag.select(".y.grid").call(this.magYGrid);
+        this.plotPhs.append("rect")
+            .attr("width", this.width)
+            .attr("height", this.height);
+    },
     
-        this.plotPhs.select(".x1.axis").call(this.phsXAxis1);
-        this.plotPhs.select(".y1.axis").call(this.phsYAxis1);
-        this.plotPhs.select(".x2.axis").call(this.phsXAxis2);
-        this.plotPhs.select(".y2.axis").call(this.phsYAxis2);
-        this.plotPhs.select(".x.grid").call(this.xGrid);
-        this.plotPhs.select(".y.grid").call(this.phsYGrid);
-    
-        this.dataMag = [];
-        this.dataPhs = [];
-        this.magDataPoints = [];
-        this.phaseDataPoints = [];
-    
-        //Push magnitude and phase data into graph.
-        for(var i = 0; i < range.length; i++){
-            this.phaseDataPoints.push({
-                x: range[i],
-                y: this.phases[i]
-            });
+    redraw: function()
+    {
+        // Get container div of graph to be re-drawn
+        var div = this.parentElement.parentElement.id;
+        var bode;
+
+        //Select object associated with graph to update.
+        graphs.forEach(function(element){
+            if(element.mag_plot_div == div || element.phase_plot_div == div){
+                bode = element;
+            }
+        });
+
+        if(bode != undefined)
+        {
+            bode.plotMag.select(".x1.axis").call(bode.magXAxis1);
+            bode.plotMag.select(".y1.axis").call(bode.magYAxis1);
+            bode.plotMag.select(".x2.axis").call(bode.magXAxis2);
+            bode.plotMag.select(".y2.axis").call(bode.magYAxis2);
+            bode.plotMag.select(".x.grid").call(bode.xGrid);
+            bode.plotMag.select(".y.grid").call(bode.magYGrid);
+
+            bode.plotPhs.select(".x1.axis").call(bode.phsXAxis1);
+            bode.plotPhs.select(".y1.axis").call(bode.phsYAxis1);
+            bode.plotPhs.select(".x2.axis").call(bode.phsXAxis2);
+            bode.plotPhs.select(".y2.axis").call(bode.phsYAxis2);
+            bode.plotPhs.select(".x.grid").call(bode.xGrid);
+            bode.plotPhs.select(".y.grid").call(bode.phsYGrid);
         
-            this.magDataPoints.push({
-                x: range[i],
-                y: this.mags[i]
-            });
+            var dataMag = [];
+            var dataPhs = [];
+            var magDataPoints = [];
+            var phaseDataPoints = [];
+        
+            //Push magnitude and phase data into graph.
+            for(var i = 0; i < range.length; i++)
+            {
+                phaseDataPoints.push({
+                    x: range[i],
+                    y: bode.phases[i]
+                });
+            
+                magDataPoints.push({
+                    x: range[i],
+                    y: bode.mags[i]
+                });
+            }
+        
+            dataMag.push({data: magDataPoints, width: 1, color: 'blue', stroke: "0,0", legend: "Magnitude" });
+        
+            seriesMag = bode.plotMag.selectAll(".line").data(dataMag);
+        
+            seriesMag.enter().append("path");
+        
+            seriesMag.attr("class","line")
+                .attr("d",function(d) { return bode.magLine(d.data); })
+                .attr("stroke-width", function(d) { return d.width; })
+                .style("stroke", function(d) { return d.color; })
+                .style("stroke-dasharray", function(d) { return d.stroke; });
+        
+            dataPhs.push({data: phaseDataPoints, width: 1, color: 'blue', stroke: "0,0", legend: "Phase" });
+        
+            seriesPhs = bode.plotPhs.selectAll(".line").data(dataPhs);
+        
+            seriesPhs.enter().append("path");
+        
+            seriesPhs.attr("class","line")
+                .attr("d",function(d) { return bode.phsLine(d.data); })
+                .attr("stroke-width", function(d) { return d.width; })
+                .style("stroke", function(d) { return d.color; })
+                .style("stroke-dasharray", function(d) { return d.stroke; });
         }
-    
-        this.dataMag.push({data: this.magDataPoints, width: 1, color: 'blue', stroke: "0,0", legend: "Magnitude" });
-    
-        this.seriesMag = this.plotMag.selectAll(".line").data(this.dataMag);
-    
-        this.seriesMag.enter().append("path");
-    
-        this.seriesMag.attr("class","line")
-            .attr("d",function(d) { return this.magLine(d.data); })
-            .attr("stroke-width", function(d) { return d.width; })
-            .style("stroke", function(d) { return d.color; })
-            .style("stroke-dasharray", function(d) { return d.stroke; });
-    
-        this.dataPhs.push({data: this.phaseDataPoints, width: 1, color: 'blue', stroke: "0,0", legend: "Phase" });
-    
-        this.seriesPhs = this.plotPhs.selectAll(".line").data(this.dataPhs);
-    
-        this.seriesPhs.enter().append("path");
-    
-        this.seriesPhs.attr("class","line")
-            .attr("d",function(d) { return this.phsLine(d.data); })
-            .attr("stroke-width", function(d) { return d.width; })
-            .style("stroke", function(d) { return d.color; })
-            .style("stroke-dasharray", function(d) { return d.stroke; });
-    
-        this.plotPhs.selectAll('.x1.axis>.tick')  // find all the x axis ticks and loop
-            .each(function(d,i){
-                if (d3.select(this).select('text').text() === ""){ // if they have no label
-                    d3.selectAll('.x.grid>.tick:nth-child(' + (i + 1) + ')') // get the corresponding grid line
-                        .style("stroke-dasharray", "3,3"); // and make it dashed
-                }
-            });
     }
 }
 
@@ -397,6 +411,9 @@ var input_output_bode = new BodePlot(mags_input_output_transfer, mags_min_input_
 var duty_output_bode = new BodePlot(mags_duty_output_transfer, mags_min_duty_output_transfer, mags_max_duty_output_transfer, 
         phases_duty_output_transfer, phase_min_duty_output_transfer, phase_max_duty_output_transfer, duty_out_mag_div, duty_out_phs_div);
 
-output_impedance_bode.redraw();
-//input_output_bode.createPlots();
-//duty_output_bode.createPlots();
+output_impedance_bode.createGraphs();
+input_impedance_bode.createGraphs();
+input_output_bode.createGraphs();
+duty_output_bode.createGraphs();
+
+graphs = Array(output_impedance_bode, input_impedance_bode, input_output_bode, duty_output_bode);
