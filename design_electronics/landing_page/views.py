3c1fd4bd4e258b32e582c6702c365a97e73f5160
Shannon <Jacob.Shannon@craneae.com>
2018-06-01 16:20:57 -0700

    synchronize commit

--- design_electronics/landing_page/views.py
+++ design_electronics/landing_page/views.py
@@ -5,19 +5,22 @@ from .models import ConverterEquation
 #####################################
 #Circuit Parameters
 #####################################
+fs = 100000
 input_voltage = 24
-output_voltage = 5
-output_current = 3
-q1_on_res = 25 #milliOhms
+output_voltage = 12
+output_current = 1
+q1_on_res = 5 #milliOhms
 d1_on_volt = 0.7
 inductor_res = 5 #milliohms
 capacitor_res = 10 #milliohms
-inductance = 10 #microhenries
-capacitance = 10 #microfarads
+inductance = round(((input_voltage-output_voltage)/((0.2*output_current)*(output_voltage/input_voltage)*(1/fs)))*1e6)
+capacitance = 100 #microfarads
 load_res = output_voltage/output_current #ohms
 
 def js_math(transfer_function):
     num_points = 1000
+
+    #print("Transfer function is: " + str(transfer_function) + "\n")
 
     start_frequency = 1 #Hz
     end_frequency = 11 #kHz
@@ -45,7 +48,7 @@ def js_math(transfer_function):
         denominator = str(1)
     
     #Print transfer function for debugging purposes.
-    print(transfer_function)
+    print("Transfer function is: " + str(transfer_function) + "\n")
 
     phases = []
     mags = []
@@ -60,17 +63,19 @@ def js_math(transfer_function):
             c_num = complex(eval(num))
             denom = denominator.replace("s", complex_replace)
             c_denom = complex(eval(denom))
-            c_transfer = c_num*c_denom/(c_denom*c_denom)
+            c_denom_conj = c_denom.conjugate()
+            c_transfer = (c_num/c_denom)*(c_denom_conj/c_denom_conj)
         else:
             c_transfer = complex(transfer_function.replace("s", complex_replace))
 
         mags.append(20*math.log10(abs(c_transfer)))
         phases.append(cmath.phase(c_transfer)*180/cmath.pi)
 
+
     #Find and print -3dB point if in list of analyzed frequencies
-    print(next((bode_x_range[mags.index(i)] for i in mags if i <= -3), 'Increase Frequency Range to see -3 dB point of Transfer Function'))
+    #print(next((bode_x_range[mags.index(i)] for i in mags if i <= -3), 'Increase Frequency Range to see -3 dB point of Transfer Function'))
     #Find and print cross over frequency if in list of analyzed frequencies
-    print(next((bode_x_range[phases.index(i)] for i in phases if i <= 0), 'Increase frequency range to see cross over frequency of transfer function'))
+    #print(next((bode_x_range[phases.index(i)] for i in phases if i <= 0), 'Increase frequency range to see cross over frequency of transfer function'))
 
     return bode_x_range, mags, phases
 
@@ -98,7 +103,7 @@ def index(request):
     #Circuit Parameters Context Update  #
     #####################################
     context.update({'input_voltage': input_voltage, 'output_voltage': output_voltage, 'output_current': output_current, 'q1_on_res': q1_on_res,
-                    'd1_on_volt': d1_on_volt, 'inductor_res': inductor_res, 'capacitor_res': capacitor_res, 'inductance': inductance,
+                    'd1_on_volt': d1_on_volt, 'inductor_res': inductor_res, 'capacitor_res': capacitor_res, 'inductance': round(inductance/1e6),
                     'capacitance': capacitance})
 
     #####################################
@@ -119,13 +124,13 @@ def index(request):
     out_imped_phs_div = "out-imped-phs-div"
     bode_x_range_output_impedance, mags_output_impedance, phases_output_impedance = js_math(output_impedance)
     phase_min_output_impedance = min(phases_output_impedance)
-    phase_min_output_impedance += round(0.5*phase_min_output_impedance)
+    phase_min_output_impedance -= round(0.5*phase_min_output_impedance)
 
     phase_max_output_impedance = max(phases_output_impedance)
     phase_max_output_impedance += round(0.5*phase_max_output_impedance)
 
     mags_min_output_impedance = min(mags_output_impedance)
-    mags_min_output_impedance += round(0.1*mags_min_output_impedance)
+    mags_min_output_impedance -= round(0.1*mags_min_output_impedance)
 
     mags_max_output_impedance = max(mags_output_impedance)
     mags_max_output_impedance += round(0.1*mags_max_output_impedance)
@@ -142,13 +147,13 @@ def index(request):
     in_imped_phs_div = "in-imped-phs-div"
     bode_x_range_input_impedance, mags_input_impedance, phases_input_impedance = js_math(input_impedance)
     phase_min_input_impedance = min(phases_input_impedance)
-    phase_min_input_impedance += round(0.5*phase_min_input_impedance)
+    phase_min_input_impedance -= round(0.5*phase_min_input_impedance)
 
     phase_max_input_impedance = max(phases_input_impedance)
     phase_max_input_impedance += round(0.5*phase_max_input_impedance)
 
     mags_min_input_impedance = min(mags_input_impedance)
-    mags_min_input_impedance += round(0.1*mags_min_input_impedance)
+    mags_min_input_impedance -= round(0.1*mags_min_input_impedance)
 
     mags_max_input_impedance = max(mags_input_impedance)
     mags_max_input_impedance += round(0.1*mags_max_input_impedance)
@@ -165,13 +170,13 @@ def index(request):
     in_out_phs_div = "in-out-phs-div"
     bode_x_range_input_output_transfer, mags_input_output_transfer, phases_input_output_transfer = js_math(input_output_transfer)
     phase_min_input_output_transfer = min(phases_input_output_transfer)
-    phase_min_input_output_transfer += round(0.5*phase_min_input_output_transfer)
+    phase_min_input_output_transfer -= round(0.5*phase_min_input_output_transfer)
 
     phase_max_input_output_transfer = max(phases_input_output_transfer)
     phase_max_input_output_transfer += round(0.5*phase_max_input_output_transfer)
 
     mags_min_input_output_transfer = min(mags_input_output_transfer)
-    mags_min_input_output_transfer += round(0.1*mags_min_input_output_transfer)
+    mags_min_input_output_transfer -= round(0.1*mags_min_input_output_transfer)
 
     mags_max_input_output_transfer = max(mags_input_output_transfer)
     mags_max_input_output_transfer += round(0.1*mags_max_input_output_transfer)
@@ -188,13 +193,13 @@ def index(request):
     duty_out_phs_div = "duty-out-phs-div"
     bode_x_range_duty_output_transfer, mags_duty_output_transfer, phases_duty_output_transfer = js_math(duty_output_transfer)
     phase_min_duty_output_transfer = min(phases_duty_output_transfer)
-    phase_min_duty_output_transfer += round(0.5*phase_min_duty_output_transfer)
+    phase_min_duty_output_transfer -= round(0.5*phase_min_duty_output_transfer)
 
     phase_max_duty_output_transfer = max(phases_duty_output_transfer)
     phase_max_duty_output_transfer += round(0.5*phase_max_duty_output_transfer)
 
     mags_min_duty_output_transfer = min(mags_duty_output_transfer)
-    mags_min_duty_output_transfer += round(0.1*mags_min_duty_output_transfer)
+    mags_min_duty_output_transfer -= round(0.1*mags_min_duty_output_transfer)
 
     mags_max_duty_output_transfer = max(mags_duty_output_transfer)
     mags_max_duty_output_transfer += round(0.1*mags_max_duty_output_transfer)
